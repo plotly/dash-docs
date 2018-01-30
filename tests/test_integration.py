46c6129bd56f0693f725512a01d200fff9755bdb
cldougl <cldougl@gmail.com>
2018-01-30 15:50:10 -0500

    search snapshot test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -88,3 +88,18 @@ class Tests(IntegrationTests):
         for link in links:
             if link.startswith('/'):
                 visit_and_snapshot(link)
+
+    def test_search(self):
+        self.startServer(app, '/dash/search')
+
+        try:
+            self.wait_for_element_by_id('wait-for-layout')
+        except Exception as e:
+            print(self.wait_for_element_by_id(
+                '_dash-app-content').get_attribute('innerHTML'))
+            raise e
+
+        search_element = self.driver.find_element_by_id('search-input')
+        search_element.clear()
+        search_element.send_keys('dropdown')
+        self.snapshot('search-dropdown')
