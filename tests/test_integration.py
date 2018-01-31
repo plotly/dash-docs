fe0854c393dc4233f123d723b7ad70d8a07b03b4
chriddyp <chris@plot.ly>
2018-01-31 11:29:21 -0500

    wait for search element first

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -44,6 +44,7 @@ class Tests(IntegrationTests):
                     return False
             wait_for(find_element_by_id)
             find_element_by_id()
+
         self.wait_for_element_by_id = wait_for_element_by_id
 
         def wait_for_element_by_css_selector(css_selector):
@@ -91,6 +92,7 @@ class Tests(IntegrationTests):
 
         # test search
         visit_and_snapshot('/dash/search')
+        wait_for_element_by_id('search-input')
         search_element = self.driver.find_element_by_id('search-input')
         search_element.clear()
         search_element.send_keys('dropdown')
