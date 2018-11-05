30698ebb193bdc92564b4a95fd532eb78ab49f3a
Brylie Christopher Oxley <brylie.oxley@maas.global>
2018-11-05 11:58:49 +0200

    Remove non-essential code examples

--- tutorial/project_structure.py
+++ tutorial/project_structure.py
@@ -32,35 +32,7 @@ The `index.py` file will be fairly minimal, containing only logic to run the app
 
     dcc.SyntaxHighlighter("""
 # Import the app and server instances
-from server import app, server
-
-# Optionally set the app title
-app.title = 'My Dash app'
-
-# Optionally override the app basic HTML structure
-app.index_string = '''
-<!DOCTYPE html>
-<html>
-    <head>
-        {%metas%}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta
-            name="description"
-            content="My Dash app is great!"
-        >
-        <title>{%title%}</title>
-        {%favicon%}
-        {%css%}
-    </head>
-    <body>
-        {%app_entry%}
-        <footer>
-            {%config%}
-            {%scripts%}
-        </footer>
-    </body>
-</html>
-'''
+from server import app
 
 # Run the app if this file is called directly
 if __name__ == '__main__':
@@ -69,7 +41,6 @@ if __name__ == '__main__':
 """, customStyle=styles.code_container),
 
     dcc.Markdown("""
-
 ## Server
 `server.py` is where we actually define the Dash app instance, and possibly grab a reference to the Flask 
 server instance:"""),
@@ -78,50 +49,19 @@ server instance:"""),
 # Required import
 from dash import Dash
 
-# Useful import
-from flask import Flask, send_from_directory
-
 # Import main layout (e.g. placeholder elements)
-from .layouts import main_layout
-
-
-# external JavaScript files
-external_scripts = [ ... ]
-
-# external CSS stylesheets
-external_stylesheets = [ ... ]
+from layouts import main_layout
 
 # Create Dash instance
 # optionally attach external scripts and stylesheets
-app = Dash(
-    __name__,
-    external_scripts=external_scripts,
-    external_stylesheets=external_stylesheets,
-)
+app = Dash()
 
 # Attach the main layout
 app.layout = main_layout
 
-# Get a reference to Flask server
-# Optional, but may be useful for routing, authentication, etc.
-server = app.server
-
-# Optional settings for local development/debugging
-app.css.config.serve_locally = True
-app.scripts.config.serve_locally = True
-
 # This is necessary to prevent warnings 
 # as callbacks will be defined in separate file
 app.config.suppress_callback_exceptions = True
-
-# Optionally add server routes, e.g.
-@server.route('/favicon.ico')
-def favicon():
-    '''Serve the favicon'''
-    return send_from_directory(
-        app._assets_folder,
-        'favicon.ico',
-        mimetype='image/x-icon')
     """, customStyle=styles.code_container),
 
 
@@ -150,6 +90,5 @@ This chapter is informed and inspired by the following resource(s) and discussio
 
 - [Splitting callback definitions in multiple files](https://community.plot.ly/t/splitting-callback-definitions-in-multiple-files/10583/2)
 - [Slapdash](https://github.com/ned2/slapdash) - boilerplate for bootstrapping scalable multi-page Dash applications
-"""
-    ),
+    """),
 ]
