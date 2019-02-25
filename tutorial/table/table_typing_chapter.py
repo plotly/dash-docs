8f454f338e2e7b8f01a9dad033a9c10dddfcbf06
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-02-25 15:32:16 -0500

    - wireframe for new table typing sub-section

---
+++ tutorial/table/table_typing_chapter.py
@@ -0,0 +1,18 @@
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+from textwrap import dedent
+
+import dash_table
+from tutorial import styles, tools
+
+
+examples = {
+
+}
+
+layout = html.Div([
+    dcc.Markdown('# DataTable - Typing'),
+
+])
