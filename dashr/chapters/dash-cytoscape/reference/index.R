b9aed58c11c0de4e5fa3188dc49b0931c484c109
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-07-02 16:46:01 -0400

    draft part 7

---
+++ dashr/chapters/dash-cytoscape/reference/index.R
@@ -0,0 +1,19 @@
+library(dash)
+library(dashCoreComponents)
+library(dashHtmlComponents)
+library(dashCytoscape)
+library(data.table)
+
+utils <- new.env()
+source('dashr/utils.R', local=utils)
+
+layout <- htmlDiv(list(
+  htmlH1("Cytoscape Reference"),
+
+  htmlH2("Properties"),
+  utils$generate_table(rbindlist(utils$props_to_list('cytoCytoscape'), fill = TRUE)),
+
+  htmlHr(),
+  dccMarkdown("[Back to Cytoscape Documentation](/dash-cytoscape)"),
+  dccMarkdown("[Back to Dash Documentation](/)")
+))
