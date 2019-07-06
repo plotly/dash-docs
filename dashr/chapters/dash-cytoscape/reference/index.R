ce8285ed224bb5ffc75ce1043ea1651d097fa5bf
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-07-06 16:25:30 -0400

    edit part 7

--- dashr/chapters/dash-cytoscape/reference/index.R
+++ dashr/chapters/dash-cytoscape/reference/index.R
@@ -10,9 +10,14 @@ source('dashr/utils.R', local=utils)
 layout <- htmlDiv(list(
   htmlH1("Cytoscape Reference"),
 
-  htmlH2("Cytoscape"),
-  htmlH3("Properties"),
+  htmlH2("Cytoscape Properties"),
+  dccMarkdown("
+  The full list of Cytoscape properties and their settings can be viewed by entering `help('cytoCytoscape')`
+  in the console. Likewise, an equivalent list for Python users can be found
+  [here](https://dash.plot.ly/cytoscape/reference).
+  "),
   utils$generate_table(rbindlist(utils$props_to_list('cytoCytoscape'), fill = TRUE)),
+  # EDIT: Complete this table! Currently incomplete.
 
   htmlHr(),
   dccMarkdown("[Back to Cytoscape Documentation](/cytoscape)"),
