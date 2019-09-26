0501242c72ca69d852325cacb5452b9fc6387adb
inytar <pieter.mulder@wooga.net>
2019-09-26 10:28:12 +0200

    Adds an example of serverside options filtering in Dropdown
    Requires the `search_value` property in `Dropdown`

---
+++ tutorial/examples/core_components/dropdown_dynamic_options.py
@@ -0,0 +1,26 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+options=[
+    {'label': 'New York City', 'value': 'NYC'},
+    {'label': 'Montreal', 'value': 'MTL'},
+    {'label': 'San Francisco', 'value': 'SF'}
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = dcc.Dropdown(id='my-dynamic-dropdown')
+
+
+@app.callback(
+    dash.dependencies.Output('my-dynamic-dropdown', 'options'),
+    [dash.dependencies.Input('my-dynamic-dropdown', 'search_value')],
+)
+def update_options(search_value):
+    return [o for o in options if search_value in o['label']]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
