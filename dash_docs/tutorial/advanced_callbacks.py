a1d6d69878f9933acdec9a08bbb5fe2596eac881
Shammamah Hossain <shammamah.hossain@gmail.com>
2020-03-11 16:35:18 -0400

    Move callback context to advanced callbacks.

--- dash_docs/tutorial/advanced_callbacks.py
+++ dash_docs/tutorial/advanced_callbacks.py
@@ -8,28 +8,17 @@ from dash_docs import reusable_components
 examples = {
     'prevent-update': tools.load_example('tutorial/examples/prevent_update.py'),
     'prevent-update-button': tools.load_example('tutorial/examples/prevent_update_button.py'),
+    'last-clicked-button': tools.load_example('tutorial/examples/last_clicked_button.py')
 }
 
 layout = html.Div([
-    html.H1('Dash PreventUpdate'),
+    html.H1('Advanced Callbacks'),
 
     reusable_components.Markdown('''
-        ## Dash State
-        <blockquote>
-        This is the 4th chapter of the <dccLink children="Dash Tutorial" href="/"/>.
-        The <dccLink href="/getting-started-part-2" children="previous chapter"/> covered Dash Callbacks
-        and the <dccLink href="/interactive-graphing" children="next chapter"/> covers interactive
-        graphing and crossfiltering.
-        Just getting started? Make sure to
-        <dccLink href="/installation" children="install the necessary dependencies"/>.
-        </blockquote>
-    '''),
+    ## Catching errors with `PreventUpdate` and `dash.no_update`
 
-
-        ## Using PreventUpdate in Callback
-    reusable_components.Markdown('''
-        In certain situations, you don't want to update the callback output. You can
-        achieve this by raising a `PreventUpdate` exception in the callback function.
+    In certain situations, you don't want to update the callback output. You can
+    achieve this by raising a `PreventUpdate` exception in the callback function.
     '''),
     Syntax(examples['prevent-update-button'][0]),
     Example(examples['prevent-update-button'][1]),
@@ -41,8 +30,37 @@ layout = html.Div([
     Syntax(examples['prevent-update'][0]),
     Example(examples['prevent-update'][1]),
 
-    dcc.Link(
-        'Dash Tutorial Part 5. Interactive Graphing',
-        href=tools.relpath('/interactive-graphing')),
+    reusable_components.Markdown('''
+    ## Determining which `Input` has fired with callback context
+    *New in v0.38.0!*
+
+    In addition to event properties like `n_clicks`
+    that change whenever an event happens (in this case a click), there is a
+    global variable `dash.callback_context`, available only inside a callback.
+    It has properties:
+    - `triggered`: list of changed properties. This will be empty on initial
+      load, unless an `Input` prop got its value from another initial callback.
+      After a user action it is a length-1 list, unless two properties of a
+      single component update simultaneously, such as a value and a timestamp
+      or event counter.
+    - `inputs` and `states`: allow you to access the callback params
+      by id and prop instead of through the function args. These have the form
+      of dictionaries `{ 'component_id.prop_name': value }`
+
+    Here's an example of how this can be done:'''),
+
+    Syntax(examples['last-clicked-button'][0]),
+    Example(examples['last-clicked-button'][1]),
+    reusable_components.Markdown('''
+
+    Prior to v0.38.0, you needed to compare timestamp properties like
+    `n_clicks_timestamp` to find the most recent click. While existing uses of
+    `*_timestamp` continue to work for now, this approach is deprecated, and
+    may be removed in a future update. The one exception is
+    `modified_timestamp` from `dcc.Store`, which is safe to use, it is NOT
+    deprecated.
+
+    ------------------------
+    ''')
 
 ])
