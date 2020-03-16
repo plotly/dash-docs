7dddbb088a6ccd5e8626c9729235d08eb69fa136
Shammamah Hossain <shammamah@plot.ly>
2020-03-16 15:40:23 -0400

    Indent JS function body.

--- dash_docs/tutorial/clientside_callbacks.py
+++ dash_docs/tutorial/clientside_callbacks.py
@@ -80,7 +80,7 @@ layout = html.Div([
     window.dash_clientside = Object.assign({}, window.dash_clientside, {
         clientside: {
             large_params_function: function(largeValue1, largeValue2) {
-            return someTransform(largeValue1, largeValue2);
+                return someTransform(largeValue1, largeValue2);
             }
         }
     });
