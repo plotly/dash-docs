9ef6d0157f96d6c8f1f99f6ff09bbdd9de2212d7
Ryan Patrick Kyle <ryan@plot.ly>
2020-05-29 14:21:39 -0400

    use dput

--- dash_docs/chapters/dash_core_components/Button/examples/button.R
+++ dash_docs/chapters/dash_core_components/Button/examples/button.R
@@ -19,10 +19,11 @@ app$callback(
   params=list(input(id = "button", property = "n_clicks"),
               input(id = "input-box", property = "value")),
   function(n_clicks, value) {
+    object_as_string <- capture.output(dput(n_clicks))
     if (is.list(n_clicks)) {
-      sprintf("'n_clicks' is a list of length %d", length(n_clicks))
+      sprintf("'n_clicks' is of length %d, with the value '%s'", length(n_clicks), object_as_string)
     } else {
-      sprintf("'n_clicks' is a vector and you have clicked the button %d time(s)", n_clicks)
+      sprintf("'n_clicks' is of length %d, and you have clicked the button %d time(s), and the value is '%s'", length(n_clicks), n_clicks, value)
     }
   }
 )
