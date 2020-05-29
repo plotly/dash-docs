440b48b5ed55fe2becdec0ce866285fc65afadb9
Ryan Patrick Kyle <ryan@plot.ly>
2020-05-29 11:18:04 -0400

    clarify example, suggested by mjkallen

--- dash_docs/chapters/dash_core_components/Button/examples/button.R
+++ dash_docs/chapters/dash_core_components/Button/examples/button.R
@@ -1,5 +1,4 @@
 library(dash)
-library(dashCoreComponents)
 library(dashHtmlComponents)
 
 app <- Dash$new()
@@ -20,7 +19,12 @@ app$callback(
   params=list(input(id = "button", property = "n_clicks"),
               input(id = "input-box", property = "value")),
   function(n_clicks, value) {
-    sprintf("The input value was \'%s\' and the button has been clicked \'%s\' times", value, n_clicks)
-  })
+    if (is.list(n_clicks)) {
+      sprintf("'n_clicks' is a list of length %d", length(n_clicks))
+    } else {
+      sprintf("'n_clicks' is a vector and you have clicked the button %d time(s)", n_clicks)
+    }
+  }
+)
 
 app$run_server()
