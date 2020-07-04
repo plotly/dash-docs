db53c6164245c49005e7a5d9da9a64a732c03c5a
John Bampton <jbampton@users.noreply.github.com>
2020-07-04 11:10:23 +1000

    Fix shell script: use #!, not just #, for the shebang.
    Checked with ShellCheck.
    
    https://www.shellcheck.net/
    
    https://github.com/koalaman/shellcheck/wiki/SC1113

--- web.sh
+++ web.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 if [ "$LANGUAGE" = "R" ]
 then fakechroot fakeroot chroot /app/.root /bin/sh -c '/usr/bin/R -f /app/run.R --gui-none --no-save'
 else gunicorn --preload index:server 
