0783e0fbc6beb9286b9f8904145dac64c12ac5f2
harrisonn <griffin.harrisonn@gmail.com>
2020-10-02 17:47:02 -0400

    use .date() for datetime objects and callback change callback string formatting

--- dash_docs/chapters/dash_core_components/DatePickerRange/examples/date_picker_range.py
+++ dash_docs/chapters/dash_core_components/DatePickerRange/examples/date_picker_range.py
@@ -10,9 +10,9 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     dcc.DatePickerRange(
         id='my-date-picker-range',
-        min_date_allowed=dt(1995, 8, 5),
-        max_date_allowed=dt(2017, 9, 19),
-        initial_visible_month=dt(2017, 8, 5),
+        min_date_allowed=dt(1995, 8, 5).date(),
+        max_date_allowed=dt(2017, 9, 19).date(),
+        initial_visible_month=dt(2017, 8, 5).date(),
         end_date=dt(2017, 8, 25).date()
     ),
     html.Div(id='output-container-date-picker-range')
@@ -26,12 +26,12 @@ app.layout = html.Div([
 def update_output(start_date, end_date):
     string_prefix = 'You have selected: '
     if start_date is not None:
-        start_date = dt.strptime(re.split('T| ', start_date)[0], '%Y-%m-%d')
-        start_date_string = start_date.strftime('%B %d, %Y')
+        start_date_object = dt.strptime(start_date, '%Y-%m-%d')
+        start_date_string = start_date_object.strftime('%B %d, %Y')
         string_prefix = string_prefix + 'Start Date: ' + start_date_string + ' | '
     if end_date is not None:
-        end_date = dt.strptime(re.split('T| ', end_date)[0], '%Y-%m-%d')
-        end_date_string = end_date.strftime('%B %d, %Y')
+        end_date_object = dt.strptime(end_date, '%Y-%m-%d')
+        end_date_string = end_date_object.strftime('%B %d, %Y')
         string_prefix = string_prefix + 'End Date: ' + end_date_string
     if len(string_prefix) == len('You have selected: '):
         return 'Select a date to see it displayed here'
