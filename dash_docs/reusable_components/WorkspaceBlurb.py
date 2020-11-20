fe3cef2e1f0786895788f5c2c7a8a1006482ed63
Xing Han Lu <xinghan@plot.ly>
2020-11-20 12:37:42 -0500

    Update WorkspaceBlurb.py

--- dash_docs/reusable_components/WorkspaceBlurb.py
+++ dash_docs/reusable_components/WorkspaceBlurb.py
@@ -1,6 +1,7 @@
 import dash_core_components as dcc
 
-WorkspaceBlurb = dcc.Markdown(
+WorkspaceBlurb = html.Div([
+  dcc.Markdown(
 '''
 > [Dash Enterprise](https://plotly.com/dash) is the fastest way to write &
 > deploy Dash apps and Jupyter notebooks. Click below to install on one
@@ -8,7 +9,12 @@ WorkspaceBlurb = dcc.Markdown(
 > - [Install Dash Enterprise on Azure](https://plotly.com/dash/azure/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=azure)
 > - [Install Dash Enterprise on AWS](https://plotly.com/dash/aws/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=aws)
 > - [Install Dash Enterprise on an on-premises Linux server](https://plotly.com/dash/on-premises-linux/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=linux)
-
-> Or, [find out if your company is using Dash Enterprise](https://go.plotly.com/company-lookup)
 '''
-)
+  ),
+  dcc.Markdown(
+'''
+> Or, [find out if your company is using Dash Enterprise](https://go.plotly.com/company-lookup)
+''',
+    className="red-links"
+  )
+])
