d57a69d9560592b589dfdd3bf710c2627d9240a9
xhlulu <xhlperso@gmail.com>
2020-11-20 17:32:20 -0500

    Update the default workspaceblurb

--- dash_docs/reusable_components/WorkspaceBlurb.py
+++ dash_docs/reusable_components/WorkspaceBlurb.py
@@ -4,16 +4,17 @@ import dash_html_components as html
 WorkspaceBlurb = html.Div([
   dcc.Markdown(
 '''
-> [Dash Enterprise](https://plotly.com/dash) is the fastest way to write &
-> deploy Dash apps and Jupyter notebooks. Click below to install on one
-> of these clouds:
-> - [Install Dash Enterprise on Azure](https://plotly.com/dash/azure/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=azure)
-> - [Install Dash Enterprise on AWS](https://plotly.com/dash/aws/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=aws)
-> - [Install Dash Enterprise on an on-premises Linux server](https://plotly.com/dash/on-premises-linux/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=linux)
-'''
-  ),
-  dcc.Markdown(
-    '> Or, [find out if your company is using Dash Enterprise](https://go.plotly.com/company-lookup)',
-    className="red-links"
+> Dash Enterprise is the fastest way to write & deploy Dash apps and 
+> Jupyter notebooks. Dash Enterprise can be installed on the Kubernetes 
+> services of 
+> [AWS](https://plotly.com/dash/aws/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=aws), 
+> [Azure](https://plotly.com/dash/azure/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=azure), 
+> [GCP](https://plotly.com/dash/on-premises-linux/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=linux), 
+> or an 
+> [on-premise Linux Server](https://plotly.com/dash/on-premises-linux/?utm_source=docs&utm_medium=workspace&utm_campaign=nov&utm_content=linux).
+> 10% of the Fortune 500 uses Dash Enterprise to productionize AI and data science apps.
+> [Find out if your company is using Dash Enterprise](https://go.plotly.com/company-lookup)
+''',
+  className="lookup-red-links",
   )
 ])
