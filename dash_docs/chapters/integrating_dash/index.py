a24e384c44195c486bdc3b800f9ed87ae3fb0487
xhlulu <xhlperso@gmail.com>
2021-01-18 17:44:53 -0500

    Change react component to a class

--- dash_docs/chapters/integrating_dash/index.py
+++ dash_docs/chapters/integrating_dash/index.py
@@ -73,18 +73,37 @@ layout = html.Div([
     window.ReactDOM = ReactDOM;
     window.PropTypes = PropTypes;
 
-    function App() {
-        return (
-            <div style={...}>
+    class App extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                sharedData: {
+                    myObject: {
+                        clicks: 0,
+                        aString: randomString(5),
+                        data: myData,
+                        multiplyFunc: (x, y) => { ... },
+                        sumFunc: (x, y) => { ... },
+                        storeDataFromDash: obj => { ... },
+                        dashAppData: {}
+                    },
+                },
+            };
+            this.clickIncrement = this.clickIncrement.bind(this);
+            ...
+        }
+
+        render() {
+            return (
+            <div className="App-Background">
                 ...
+                <div className="App-Content">
                 <h1>Embedded Dash Application</h1>
-                <DashApp
-                    config={{
-                        url_base_pathname: "http://dash.tests:8050",
-                    }}
-                />
+                <DashApp config={{url_base_pathname: "http://dash.tests:8050"}} value={this.state.sharedData} />
+                </div>
             </div>
-        );
+            );
+        }
     }
     ```
 
