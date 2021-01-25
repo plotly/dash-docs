11235310a83b3a94bb2b67d4cc688f0c7415bba5
Xing Han Lu <xinghan@plot.ly>
2021-01-25 11:39:30 -0500

    Adding snippet and a GIF demo about dash-embedded in /integrating-dash (#1073)
    * Adding dash-embedded GIF+snippet
    
    * Change order for better logic
    
    * Slightly fix description
    
    * Change react component to a class
    
    * Add callbacks to integrating-dash
    
    * trigger build
    
    * trigger build
    
    Co-authored-by: Joseph Damiba <joseph.damiba@plot.ly>

--- dash_docs/chapters/integrating_dash/index.py
+++ dash_docs/chapters/integrating_dash/index.py
@@ -9,7 +9,7 @@ from dash_docs import reusable_components as rc
 
 layout = html.Div([
     rc.Markdown(
-    """
+        """
     # Embedding Dash Apps in other Web Platforms
 
     Our recommend method for securely embedding Dash applications in existing
@@ -48,6 +48,134 @@ layout = html.Div([
     `https://<your-dash-enterprise-hostname>/Docs/embedded-middleware`**,
     replacing `<your-dash-enterprise-hostname>` with the hostname of your
     licensed Dash Enterprise in your VPC. [Look up the hostname for your company's license](https://go.plotly.com/company-lookup)
+    
+    #### Sharing State between a Javascript Host app and an embedded Dash App
+
+    You can find here a simple example illustrating multidirectional shared state, enabling the communication between your JavaScript host app and your Dash app.
+
+    ![GIF showing how to use dash embedded](https://raw.githubusercontent.com/plotly/dash-docs/master/images/dash-embedded-js-host.gif)
+
+    Inside your JavaScript host app, you simply provide the array or object that you want to share to your Dash app as a positional argument in Dash Embedded Components `renderDash()` function:
+    ```js
+    ...
+    var setter = window.dash_embedded_component.renderDash(
+        { url_base_pathname: "http://dash.tests:8050" }, 
+        'dash-app', 
+        sharedData
+    );
+    ```
+
+    If you are using a React app, you can import the component and use it inside JSX:
+    ```js
+    import { DashApp } from "dash-embedded-component";
+
+    window.React = React;
+    window.ReactDOM = ReactDOM;
+    window.PropTypes = PropTypes;
+
+    class App extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                sharedData: {
+                    myObject: {
+                        clicks: 0,
+                        aString: randomString(5),
+                        data: myData,
+                        multiplyFunc: (x, y) => { ... },
+                        sumFunc: (x, y) => { ... },
+                        storeDataFromDash: obj => { ... },
+                        dashAppData: {}
+                    },
+                },
+            };
+            this.clickIncrement = this.clickIncrement.bind(this);
+            ...
+        }
+
+        render() {
+            return (
+            <div className="App-Background">
+                ...
+                <div className="App-Content">
+                <h1>Embedded Dash Application</h1>
+                <DashApp config={{url_base_pathname: "http://dash.tests:8050"}} value={this.state.sharedData} />
+                </div>
+            </div>
+            );
+        }
+    }
+    ```
+
+    Then inside your Dash app, simply use the `dash_embedded.ConsumerContext` and `dash_embedded.ConsumerFunction` components consume and use the shared data:
+
+    ```python
+    ...
+    app.layout = ddk.App(
+        [
+            ...
+            ddk.Card(
+                [
+                    ddk.CardHeader(title="Triggering Callbacks from Dash App & Host App"),
+                    ConsumerContext(id="clicks", path=["myObject", "clicks"]),
+                    ConsumerContext(id="data-one", path=["myObject", "data", "dataOne"]),
+                    ConsumerContext(id="data-two", path=["myObject", "data", "dataTwo"]),
+                    ...
+                ],
+                width=50,
+            ),
+            ddk.ControlCard(
+                [
+                    ddk.CardHeader(title="Triggering Host App Functions from Dash App"),
+                    ConsumerFunction(
+                        id="host-app-multiply", path=["myObject", "multiplyFunc"]
+                    ),
+                    ConsumerFunction(id="host-app-sum", path=["myObject", "sumFunc"]),
+                    ddk.ControlItem(...),
+                    ...
+                ],
+                width=50,
+            ),
+            ...
+        ]
+    )
+    ...
+
+    # Access the nested objects via `path=["myObject"]`
+    @app.callback(Output("display-full-object", "children"), Input("full-object", "value"))
+    def display(value):
+        return json.dumps(value, indent=2)
+
+
+    # Access nested values via `path=[...]`
+    @app.callback(
+        Output("display-data_dataOne_y[1]", "children"), Input("data_dataOne_y[1]", "value"))
+    def display(value):
+        return "data.dataOne.y[1]={}".format(value)
+
+
+    # Trigger Callback from Host App Data & Dash App Buttons
+    @app.callback(
+        Output("plot", "figure"),
+        Input("update", "n_clicks"), Input("clicks", "value"),
+        State("data-one", "value"), State("data-two", "value"),
+    )
+    def update_figure(clicks_dash, clicks_host, trace1, trace2):
+        ...
+        return go.Figure(...)
+
+
+    # Trigger Host App functions by sending data into the `params` property
+    @app.callback(
+        Output("host-app-sum", "params"),
+        Input("sum", "n_clicks"),
+        State("input-x", "value"), State("input-y", "value"),
+    )
+    def trigger_sum(_, x, y):
+        return [x, y]
+    ...
+    ```
+    
 
     ## Embedding Public Apps in Public Websites with `<iframe>`
 
@@ -65,7 +193,7 @@ layout = html.Div([
     Dash app in a specific location within an existing web page with your
     desired dimensions:"""),
     rc.Markdown(
-    '''
+        '''
     ```html
     <iframe src="http://localhost:8050" width=700 height=600>
     ```
