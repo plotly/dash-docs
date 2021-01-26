975e223d257405e4e8adaa069483a39c12ee8984
Joel Ostblom <joel.ostblom@gmail.com>
2021-01-25 21:19:42 -0800

    Remove default port option

--- dash_docs/chapters/deployment/index.R
+++ dash_docs/chapters/deployment/index.R
@@ -93,7 +93,7 @@ app$callback(output=list(id='display-value', property='children'),
   }
 )
 
-app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
+app$run_server(host = '0.0.0.0')
 ```
 
 ---
