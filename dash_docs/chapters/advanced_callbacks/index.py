64541563fe76dc06d939d41560f11fd3773557e5
Joseph Damiba <Joseph.damiba@plot.ly>
2021-01-26 13:31:11 -0500

    Update dash_docs/chapters/advanced_callbacks/index.py
    Co-authored-by: Ryan Patrick Kyle <rpkyle@users.noreply.github.com>

--- dash_docs/chapters/advanced_callbacks/index.py
+++ dash_docs/chapters/advanced_callbacks/index.py
@@ -153,7 +153,7 @@ def slow_function(input):
 
     You can use the `prevent_initial_call` attribute to prevent callbacks from firing when their inputs initially appear in the layout of your Dash application.
 
-    This applies to both when a Dash app is initially loaded with a layout and when layout components are added to a layout as the result of a callback.
+    This attribute applies when the layout of your Dash app is initially loaded, and also when new components are introduced into the layout when a callback has been triggered.
 
     '''),
 
