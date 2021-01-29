0555cb2eea413aea933616cf11a440b434049018
Chris Parmer <chris@plot.ly>
2021-01-28 23:11:07 -0500

    update circular callback examples

--- dash_docs/chapters/advanced_callbacks/examples/circular_slider_input.py
+++ dash_docs/chapters/advanced_callbacks/examples/circular_slider_input.py
@@ -3,44 +3,31 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash(__name__)
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
-app.layout = html.Div(children=[
-    dcc.Slider(
-        id='slider-circular',
-        min=0, max=20,
-    ),
-    dcc.Input(
-        id='input-circular',
-        type='number',
-        debounce=False,
-        value=3
-    ),
-])
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
-@app.callback(
-    Output('input-circular', 'value'),
-    Output('slider-circular', 'value'),
-    Input('input-circular', 'value'),
-    Input('slider-circular', 'value'),
+app.layout = html.Div(
+    [
+        dcc.Slider(
+            id="slider", min=0, max=20, 
+            marks={i: str(i) for i in range(21)}, 
+            value=3
+        ),
+        dcc.Input(
+            id="input", type="number", min=0, max=20, value=3
+        ),
+    ]
 )
-def callback(iv, sv):
+@app.callback(
+    Output("input", "value"),
+    Output("slider", "value"),
+    Input("input", "value"),
+    Input("slider", "value"),
+)
+def callback(input_value, slider_value):
     ctx = dash.callback_context
-    if not ctx.triggered:
-        # default to input value during initialization
-        if iv is not None:
-            value = iv
-        else:
-            value = sv
-    else:
-        trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-        if trigger_id == 'input':
-            value = iv
-        else:
-            value = sv
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "input" else slider_value
     return value, value
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app.run_server(debug=True)
