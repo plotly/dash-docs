70cc1865bb080fcc62057a29b8b105a1e083dafc
Jack Parmer <jparmer09@gmail.com>
2021-04-12 16:05:42 -0700

    Update Pricing and Announcements links

--- dash_docs/run.py
+++ dash_docs/run.py
@@ -53,8 +53,8 @@ header = html.Div(
             # make sure to check that the responsive design still works
             # The breakpoints are set in override.css
             html.Div(className='links', children=[
-                html.A('Pricing', className='links--announcements', href='https://plotly.com/dash#pricing'),
-                html.A('Announcements', className='links--announcements', href='https://community.plotly.com/tag/announcements'),
+                html.A('Pricing', className='links--announcements', href='https://plotly.com/get-pricing/'),
+                html.A('📰 News', className='links--announcements', href='https://community.plotly.com/tag/announcements'),
                 html.A('Gallery', className='links--gallery', href='https://dash-gallery.plotly.host'),
                 html.A('Show & Tell', className='links--show-and-tell', href='https://community.plotly.com/tag/show-and-tell'),
                 html.A('Community', className='links--community-forum', href='https://community.plotly.com/c/dash'),
