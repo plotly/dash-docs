a55a462cd6aba821af40a7a322c8020bc3b35a55
Jack Parmer <jparmer09@gmail.com>
2021-04-12 18:07:13 -0700

    add newline

--- dash_docs/reusable_components/WorkspaceBlurb.py
+++ dash_docs/reusable_components/WorkspaceBlurb.py
@@ -5,6 +5,7 @@ WorkspaceBlurb = html.Div([
     html.Blockquote([dcc.Markdown(
         '''
         Write, deploy, and scale Dash apps on a Dash Enterprise Kubernetes cluster.
+        
         [Pricing](https://plotly.com/get-pricing/) | 
         [Dash Enterprise Demo](https://plotly.com/get-demo/) | 
         [Dash Enterprise Overview](https://plotly.com/dash/)
