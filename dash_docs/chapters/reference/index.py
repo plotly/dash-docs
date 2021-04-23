a4baf2c62480b700d298acf4f7f639aa60a0f2b8
Chris Parmer <chris@plot.ly>
2021-04-22 20:51:35 -0700

    reference

--- dash_docs/chapters/reference/index.py
+++ dash_docs/chapters/reference/index.py
@@ -1,28 +1,13 @@
-import dash_html_components as html
-import dash_core_components as dcc
-import dash
-
-METHODS = [
-    method
-    for method in dir(dash)
-    if not method.startswith("_")
-]
-
-
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import textwrap
 import re
-
-def convert_method_name_to_markdown(obj, method):
-    method_name = f'{obj}.{method}'
-    method_name = method_name.replace("<class 'dash.dash.Dash'>", 'app')
-    return method_name
+import inspect
 
 def convert_docstring_to_markdown(docstring):
     if not docstring:
-        return '\n\nUndocumented\n'
+        return '\n\(Not officially documented)\n'
 
     lines = docstring.split('\n')
 
@@ -43,41 +28,110 @@ def convert_docstring_to_markdown(docstring):
 
 app = dash.Dash(__name__)
 
-SKIP = ['dash']
+SKIP = ['dash', 'json', 'dedent']
+
+def doc_signature(obj, method, prefix):
+    try:
+        signature = str(inspect.signature(getattr(obj, method)))
+    except:
+        signature = ''
+
+    try:
+        name = method.__name__
+    except:
+        name = str(method)
+
+    if not prefix:
+        try:
+            prefix = obj.__name__
+        except:
+            prefix = str(obj)
+    prefix_signature = prefix
+
+    return html.Div(
+        [
+            html.H2(
+                html.Code("{}.{}".format(prefix, name)),
+                className="docs-article",
+            ),
+            html.Pre(
+                className="docs-article",
+                children=html.Code(
+                    "{}.{}{}".format(
+                        prefix_signature,
+                        name,
+                        signature
+                        .replace(",", ",\n   ")
+                        .replace("(", "(\n    ")
+                        .replace(")", "\n)")
+                        .replace("    self,\n", "")
+                        .replace("\n    self\n", ""),
+                    )
+                ),
+            ),
+        ]
+    )
 
 PUBLIC_API = [
-    dash.Dash,
-    dash.resources,
-    dash.exceptions,
-    dash.dependencies,
-    dash
+    # app,
+    # dash.Dash,
+    # dash.resources,
+    dict(obj=dash, prefix='', skip=[], preamble=dcc.Markdown(
+    '''
+    # dash
+    ```
+    import dash
+    ```
+    '''
+    ),
+    dict(obj=app, prefix='app', skip=[
+        'css',
+        'dependencies',
+        'dispatch',
+        'logger',
+        'registered_paths',
+        'renderer',
+        'routes',
+        'scripts',
+        'serve_component_suites',
+        'serve_layout',
+        'serve_reload_hash',
+        'validation_layout'
+    ], preamble=dcc.Markdown(
+    '''
+    # app
+    ```
+    import dash
+    app = dash.Dash(__name__)
+    ```
+    '''
+    )),
+    dict(obj=dash.dependencies, prefix='', skip=[], preamble=dcc.Markdown(
+    '''
+    # dash.dependencies
+    
+    The classes in `dash.dependencies` are all used in the `app.callback`
+    signature.
+    '''
+    )),
+    dict(obj=dash.exceptions, prefix='', skip=[], preamble=dcc.Markdown(
+    '''
+    # dash.exceptions
+    
+    Dash will raise exceptions under certain scenarios. 
+    Dash will always use a special exception class that can be caught to 
+    handle this particular scenario.
+    These exception classes are in this module.
+    '''
+    ))
 ]
 
-DOCSTRINGS = ''
-def recursively_create_docstrings(obj):
-    docstring = ''
-    for method in dir(obj):
-        if (obj and not method.startswith('_') and 
-                method not in SKIP and 
-                'dash' in str(obj) and 
-                'class' not in method and
-                'development' not in method):
-            print(str(obj))
-            print(method)
-            try:
-                getattr(obj, method).__doc__
-            except:
-                print(f'Error accessing {obj}.{method}')
-                continue
-            docstring = docstring + f'\n\n***\n\n**`{convert_method_name_to_markdown(obj, method)}`**\n\n' + (convert_docstring_to_markdown(getattr(obj, method).__doc__)) 
-            docstring = docstring + recursively_create_docstrings(getattr(obj, method))
-                
-    return docstring
 
 def public_methods(obj):
     methods = []    
     for method in dir(obj):
-        if (obj and not method.startswith('_') and 
+        if (obj and
+                not method.startswith('_') and 
                 method not in SKIP and 
                 'dash' in str(obj) and 
                 'class' not in method and
@@ -86,13 +140,30 @@ def public_methods(obj):
     return methods
 
 def create_docstrings():
-    docstring = ''
-    for obj in PUBLIC_API:
-        for method in public_methods(obj):
-            docstring = docstring + f'\n\n***\n\n**`{convert_method_name_to_markdown(obj, method)}`**\n\n' + (convert_docstring_to_markdown(getattr(obj, method).__doc__)) 
+    docstring = []
+    for docitem in PUBLIC_API:
+        for method in public_methods(docitem['obj']):
+            if method not in docitem['skip']:
+                docstring.append(doc_signature(docitem['obj'], method, docitem['prefix']))
+                docstring.append(docitem['preamble']),
+                docstring.append(dcc.Markdown(convert_docstring_to_markdown(getattr(docitem['obj'], method).__doc__)))
     return docstring          
 
 layout = html.Div([
 
-    dcc.Markdown(create_docstrings()),
+    dcc.Markdown(
+    '''
+    # API Reference
+    
+    This page displays the docstrings for the public methods of the
+    `dash` module including the `app` object.
+    
+    Curious about the implementation details? 
+    [Browse the Dash source code](https://github.com/plotly/dash).
+    '''
+    ),
+
+    html.Div(create_docstrings()),
+
 ])
+
